499cb4139a99550a0c3126cad8bcd12087133ec5
darren <darren@darrenlawton.com>
2021-09-26 17:39:13 +1000

    minor push

--- playground.py
+++ playground.py
@@ -1,4 +1,6 @@
 # https://medium.com/@_chriz_/development-of-a-structure-aware-pdf-parser-7285f3fe41a9
+# Raw Data > Parsing > Modelling > Application
+
 # 'artefacts/APS_113_January_2013.pdf'
 
 from pdfminer import high_level
