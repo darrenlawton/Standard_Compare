9c7cb77b5ca524f925a697f144f6f44280a8862a
Darren Lawton <darren@darrenlawton.com>
2022-08-16 12:22:45 +1000

    TOC parse complete.

--- parse_contents.py
+++ parse_contents.py
@@ -1,3 +1,4 @@
+from queue import Empty
 import re
 import unicodedata
 from difflib import SequenceMatcher
@@ -17,15 +18,16 @@ def validate_string():
     return None
 
 
-def parse_contents(pdf_file, content_pages: list):
-    contexts = {}
+def parse_contents_page(pdf_file, content_pages: list):
+    contents = {}
 
     # Get header and footer boundaries
     margins = get_margins(pdf_file, content_pages[0])
 
+    # Parse contents page, and extract topic and page number
     for page_layout in extract_pages(pdf_file=pdf_file, laparams=LAParams(line_margin=line_margin),
                                      page_numbers=content_pages):
-        multi_line = False
+        content = ''
         for element in page_layout:
             if margins['header'] > element.y0 > margins['footer'] \
                     and isinstance(element, LTTextContainer) and element.get_text().strip() != ""\
@@ -35,11 +37,19 @@ def parse_contents(pdf_file, content_pages: list):
                 check_list = [unicodedata.normalize("NFKD", x).strip() for x in
                               element.get_text().strip().split(".") if x != '']
 
-                content = re.sub(r"\d+", "", element.get_text().replace(".", ""))
+                # Remove special characters
+                content = content + re.sub('[^A-Za-z]+', "", element.get_text().replace(".", ""))
                 page_number = re.findall(r'\b\d+\b', element.get_text().replace(".", ""))
 
+                # Assess multi line topics
+                if page_number:
+                    contents[page_number[-1]] = content
+                    content = '' 
 
+                #print("{} on {}".format(content, page_number))
+    
+    return contents
 
-parse_contents(pdf_file='artefacts/reduced_APS_113_January_2013.pdf', content_pages=[1])
-#parse_contents(pdf_file='artefacts/rbnz_tableofcontents.pdf', content_pages=[0,1])
-#parse_contents(pdf_file='artefacts/boe_test_doc.pdf',  content_pages=[2])
+#parse_contents_page(pdf_file='artefacts/reduced_APS_113_January_2013.pdf', content_pages=[1])
+parse_contents_page(pdf_file='artefacts/rbnz_tableofcontents.pdf', content_pages=[0,1])
+#parse_contents_page(pdf_file='artefacts/boe_test_doc.pdf',  content_pages=[2])
